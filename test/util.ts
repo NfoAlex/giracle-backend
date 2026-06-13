137412aa5bf35374960c4dbffae08099b3e086a8
NfoAlex <40430040+NfoAlex@users.noreply.github.com>
2026-06-13 16:04:06 +0900

    change - test用のFETCHをformDataに対応

--- test/util.ts
+++ test/util.ts
@@ -156,32 +156,23 @@ export async function FETCH({
 }: {
   path: `/${string}`;
   method: "GET" | "POST" | "PUT" | "DELETE";
-  // biome-ignore lint/suspicious/noExplicitAny: データの型は不定
-  body?: { [key: string]: any };
+  // biome-ignore lint/suspicious/noExplicitAny: for test
+  body?: any;
   useSecondaryUser?: boolean;
   excludeCredential?: boolean;
 }): Promise<Response> {
-  //第２ユーザーのトークンを使うかどうかで切り替え
   const tokenUsing = useSecondaryUser ? "TESTUSER2TOKEN" : "TESTUSERTOKEN";
+  const isFormData = body instanceof FormData;
 
-  return await app
-    .handle(
-      new Request(`http://localhost${path}`, {
-        method,
-        credentials: "include",
-        headers: {
-          "Content-Type": "application/json",
-          //トークンを使用するかどうかで切り替え
-          Cookie: excludeCredential ? "" : `token=${tokenUsing}`,
-        },
-        body: JSON.stringify(body),
-      }),
-    )
-    .then(async (response) => {
-      return response;
-    })
-    .catch((error) => {
-      //console.error("FETCH error:", error);
-      throw error;
-    });
+  return await app.handle(
+    new Request(`http://localhost${path}`, {
+      method,
+      credentials: "include",
+      headers: {
+        ...(isFormData ? {} : { "Content-Type": "application/json" }),
+        Cookie: excludeCredential ? "" : `token=${tokenUsing}`,
+      },
+      body: isFormData ? body : JSON.stringify(body),
+    }),
+  );
 }
