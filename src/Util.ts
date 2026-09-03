fc553f4a88f0c4a05fef84b020ee5ab60ee9c591
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2026-09-03 20:57:36 +0900

    fix - やべ

--- src/Util.ts
+++ src/Util.ts
@@ -5,6 +5,7 @@ import CalculateRoleLevel from "./Utils/CalculateRoleLevel";
 import CheckChannelVisibility from "./Utils/CheckChannelVisibility";
 import CompareRoleLevelToRole from "./Utils/CompareRoleLevelToRole";
 import EscapeLikePattern from "./Utils/EscapeLikePattern";
+import { ExtractValidPreviewUrls } from "./Utils/ExtractValidPreviewUrls";
 import GetSafeFileExtension from "./Utils/GetSafeFileExtension";
 import GetUrlPreviewThumbnailFileName from "./Utils/GetUrlPreviewThumbnailFileName";
 import GetUserViewableChannel from "./Utils/GetUserViewableChannel";
@@ -19,6 +20,7 @@ export namespace Util {
   export const checkChannelVisibility = CheckChannelVisibility;
   export const compareRoleLevelToRole = CompareRoleLevelToRole;
   export const escapeLikePattern = EscapeLikePattern;
+  export const extractValidPreviewUrls = ExtractValidPreviewUrls;
   export const getUserViewableChannel = GetUserViewableChannel;
   export const sendPushNotification = SendPushNotification;
   export const sendSystemMessage = SendSystemMessage;
