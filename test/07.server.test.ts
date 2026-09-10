c1ea8f89c22b2c6fa8ee5a861e5b06b7fe9e36e8
Alex.exe <40430040+NfoAlex@users.noreply.github.com>
2026-09-11 00:56:49 +0900

    change - テスト更新

--- test/07.server.test.ts
+++ test/07.server.test.ts
@@ -365,9 +365,8 @@ describe("GET /server/bot/me/:botId", () => {
       path: "/server/bot/me/TESTBOT3",
       method: "GET",
     });
-    const j = await res.json();
-    expect(res.ok).toBe(true);
-    expect(j.data).toBeUndefined();
+    expect(res.status).toBe(403);
+    expect(await res.text()).toBe("You are not owner of this bot");
   });
 
   it("存在しないBot", async () => {
@@ -375,9 +374,8 @@ describe("GET /server/bot/me/:botId", () => {
       path: "/server/bot/me/TESTBOT999",
       method: "GET",
     });
-    const j = await res.json();
-    expect(res.ok).toBe(true);
-    expect(j.data).toBeUndefined();
+    expect(res.status).toBe(404);
+    expect(await res.text()).toBe("Bot not found");
   });
 });
 
